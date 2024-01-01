4f203d5fa6a9a8d01a9caf90f779e497ff0fc59e
Juan <juangui9508@hotmail.com>
2024-01-01 16:22:04 -0500

    function-clean

--- Server/src/controllers/getCharacters.js
+++ Server/src/controllers/getCharacters.js
@@ -1,13 +1,29 @@
 // const URL = "https://rickandmortyapi.com/api/character/";
 const axios = require("axios");
 
+const cleanArray = (characters) => {
+  const newArray = characters.map((character) => {
+    return {
+      id: character.id,
+      name: character.name,
+      species: character.species,
+      origin: character.origin,
+      image: character.image,
+      gender: character.gender,
+      status: character.status,
+    };
+  });
+  return newArray;
+};
+
 const getCharacters = async () => {
   const characters = (
     await axios.get("https://rickandmortyapi.com/api/character/")
-  ).data;
-  console.log(characters);
+  ).data.results;
 
-  return characters;
+  const results = cleanArray(characters);
+
+  return results;
 };
 
 module.exports = {
